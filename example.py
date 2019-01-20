077429aa5b85067616f98fe1d01d4e52f8a4eb4f
Brett Morris <brettmorris21@gmail.com>
2019-01-19 20:29:55 -0800

    Fixing example

--- example.py
+++ example.py
@@ -7,8 +7,8 @@ fig, ax = plt.subplots()
 # print(Filter.available_filters())
 
 z = Filter.from_name('SDSS_z')
-target = Target(magnitude=18.091, filter=z)
-telescope = Telescope(aperture_diameter=3.5*u.m, throughput=0.5)
+target = Target(magnitude=21.42, filter=z)
+telescope = Telescope(aperture_diameter=3.5*u.m, throughput=0.9)
 arctic = Imager(plate_scale=0.114*u.arcsec, seeing=1*u.arcsec, binning=3)
 
 exposure_duration = 45 * u.s
